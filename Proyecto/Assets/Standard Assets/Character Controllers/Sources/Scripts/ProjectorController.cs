33553d2cad8c953bd035ab01eeb22281491bdc61
JGFranco <jose.franco@outlook.com>
2014-02-21 13:32:29 -0600

    Proyecto de Unity

---
+++ Proyecto/Assets/Standard Assets/Character Controllers/Sources/Scripts/ProjectorController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectorController : MonoBehaviour {
+
+	public Light projector;
+	bool inRange;
+
+	// Use this for initialization
+	void Start () {
+		inRange = false;
+		Debug.Log ("Start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (inRange && Input.GetKey (KeyCode.E)) 
+		{
+			projector.light.enabled = !projector.light.enabled;
+		}
+	}
+
+	void onTriggerEnter(Collider other)
+	{
+		inRange = true;
+		Debug.Log ("In Range");
+	}
+
+	void onTriggerExit(Collider other)
+	{
+		inRange = false;
+	}
+}
