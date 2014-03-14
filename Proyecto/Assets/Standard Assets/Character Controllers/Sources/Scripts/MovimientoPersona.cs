4220c67d21a295509521be229837a21f628a7c8f
JGFranco <jose.franco@outlook.com>
2014-03-14 14:27:42 -0600

    Agregado el script de movimiento de personas

--- Proyecto/Assets/Standard Assets/Character Controllers/Sources/Scripts/MovimientoPersona.cs
+++ Proyecto/Assets/Standard Assets/Character Controllers/Sources/Scripts/MovimientoPersona.cs
@@ -4,24 +4,43 @@ using System.Collections;
 public class MovimientoPersona : MonoBehaviour {
 
 	public GameObject[] positions;
-	public float timeSit;
+	
 	Time t;
+	public float timeSit;
+	public float timeScale;
+
 	int currentObject;
 	int nextObject;
+
 	bool direction; //Va entrando o va saliendo
 	bool moving; //Indica si esta sentado
 
+	GameObject person;
+	Vector3 position;
+
 	// Use this for initialization
 	void Start () {
-	
+		person = this.gameObject;
+		position.y = person.transform.position.y;
+		nextObject = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		/*if (moving && this.gameObject.transform.position == gameObject[nextObject].transform.position && nextObject == positions.GetLength-1) {//Llegamos al ultimo punto
-			moving = false;
-			direction = 1;
+		if (decimal.Round((decimal)person.transform.position.x, 2) == decimal.Round((decimal)positions[nextObject].transform.position.x, 2)
+		    && decimal.Round((decimal)person.transform.position.z, 2) == decimal.Round((decimal)positions[nextObject].transform.position.z, 2)) {
+			nextObject++;
+		} else {
+			position.x = interpolate(person.transform.position.x, positions [nextObject].transform.position.x, timeScale);
+			position.z = interpolate(person.transform.position.z, positions [nextObject].transform.position.z, timeScale);
+			person.transform.position = position;
 		}
-		*/
+	}
+
+	float interpolate(float a,float b,float mu)
+	{
+		double mu2;
+		mu2 = (1- Mathf.Cos((float)mu*Mathf.PI))/2;
+		return (float)(a*(1-mu2)+b*mu2);
 	}
 }
